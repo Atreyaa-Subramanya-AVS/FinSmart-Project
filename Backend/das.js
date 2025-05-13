431121e3f8130713d5f3f25a47183846b5080396
kruthik227 <kruthiks.btech23@rvu.edu.in>
2025-05-13 09:47:34 +0530

    commit

--- Backend/das.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config({ path: require('path').resolve(__dirname, '.env') });
-
-console.log(process.env); // to see everything loaded
-
-console.log(process.env.PORT); // to see just the port  
